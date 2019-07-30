a8c43d6600952379fedffd0ddcfb09186357f3b1
jpier2012 <jpier2012@gmail.com>
2019-07-30 11:36:05 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector);
+}
+
+function nestedTarget() {
+    return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+    return document.querySelector('#grand-node div div div div');
+}
+
+function increaseRankBy(n) {
+    const list = document.querySelectorAll("#app .ranked-list li");
+    for (const element of list) {
+        element.innerHTML = (parseInt(element.innerHTML) + n).toString();
+    }
+}
